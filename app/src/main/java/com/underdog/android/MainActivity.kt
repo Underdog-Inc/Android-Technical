99cc5aca03fecdf424554d0d421bae26db9c4c0c
tlind-ud <91902752+tlind-ud@users.noreply.github.com>
2024-01-08 11:22:46 -0800

    small change

--- app/src/main/java/com/underdog/android/MainActivity.kt
+++ app/src/main/java/com/underdog/android/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -31,6 +32,6 @@ class MainActivity : AppCompatActivity() {
 
 @Preview
 @Composable
-fun LoadingSpinner() {
+fun LoadingSpinner(modifier: Modifier = Modifier) {
     CircularProgressIndicator()
 }
